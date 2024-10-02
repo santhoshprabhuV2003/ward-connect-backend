ecd83c1cfaf0b88e46b5e39ce6de0cd7a11c33ae
santhoshprabhuV2003 <santhoshprabhuofficial@gmail.com>
2024-10-02 11:45:31 +0530

    modified mongodb uri

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI)
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ward-connect';
+
+mongoose.connect(MONGO_URI)
 .then(() => {
     console.log("MongoDB Connected Successfully!!");
 }).catch((e) => {
